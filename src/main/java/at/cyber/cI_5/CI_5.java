4f3d2d5903169eb37ce4ffb3ad80407d6907bcd1
there <theredminer.gaming@gmail.com>
2026-04-12 18:17:24 +0200

    Initial commit

---
+++ src/main/java/at/cyber/cI_5/CI_5.java
@@ -0,0 +1,17 @@
+package at.cyber.cI_5;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CI_5 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
